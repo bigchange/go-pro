98adc3e2e6d5da3e34481eb87944881d1fceafd9
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-08-04 15:38:02 +0800

    add text parser

--- src/web/services/json_parser.go
+++ src/web/services/json_parser.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Server struct {
+	ServerName string
+	ServerIP   string
+}
+
+type Serverslice struct {
+	Servers []Server
+}
+
+func jsonParser() {
+	var s Serverslice
+	str := `{"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+	json.Unmarshal([]byte(str), &s)
+	fmt.Println(s)
+}
