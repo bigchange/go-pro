8da69c2c1c892298dc779aea2ea24d3501093b39
shanghai-Jerry <cjyou20110920sspu@163.com>
2018-06-14 12:05:02 +0800

    update leveldb

---
+++ myproject/gotest/level_db_test.go
@@ -0,0 +1,32 @@
+package gotest
+
+import (
+	"fmt"
+	"testing"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+
+
+func TestLevelDB(t *testing.T) {
+	db, err := leveldb.OpenFile("../data/leveldb", nil)
+	if err != nil {
+		fmt.Println("get data error", err.Error())
+	}
+	// Remember that the contents of the returned slice should not be modified.
+	data, err := db.Get([]byte("key"), nil)
+	if err == nil {
+		fmt.Println("get data:", string(data))
+	} else {
+		fmt.Println("get data error", err.Error())
+	}
+	err = db.Put([]byte("key"), []byte("value"), nil)
+	data, err = db.Get([]byte("key"), nil)
+	if err == nil {
+		fmt.Println("get data:", string(data))
+	} else {
+		fmt.Println("get data error", err.Error())
+	}
+	err = db.Delete([]byte("key"), nil)
+	fmt.Println("get data:", string(data))
+}
