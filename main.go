ef1248fd2d14985a0fbbd83e022b5817adb0ce64
shanghai-Jerry <cjyou20110920sspu@163.com>
2019-07-19 16:06:27 +0800

    add tfmodel load in golang

--- main.go
+++ main.go
@@ -1,133 +1,68 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
 
-	"github.com/bigchange/go-pro/myproject/jwt"
-	"github.com/bigchange/go-pro/myproject/utils"
-	"github.com/gin-gonic/gin"
-	// "github.com/bigchange/go-pro/myproject/db"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
 )
 
-//CORSMiddleware ...
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// 跨域配置
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONS")
-			c.AbortWithStatus(200)
-		} else {
-			c.Next()
-		}
-	}
+// TfModel ...
+type TfModel struct {
+	Model *tf.SavedModel
 }
 
-// handler request
-func helloGinHandler(c *gin.Context) {
-	c.JSON(200, "hello world!!")
+// Init ...
+func Init(path string, tags []string) *TfModel {
+	model, err := tf.LoadSavedModel(path, tags, nil)
+	if err != nil {
+		fmt.Printf("Error loading Saved Model:%v", err.Error())
+		return nil
+	}
+	return &TfModel{Model: model}
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+// Eval ...
+func (m *TfModel) Eval(inputTensor []*tf.Tensor, inputName []string, outNames string, targets []*tf.Operation) (ret []*tf.Tensor, err error) {
+	feeds := make(map[tf.Output]*tf.Tensor)
+	if len(inputTensor) != len(inputName) {
+		return ret, errors.New("input length not match")
 	}
+	for i, tensor := range inputTensor {
+		feeds[m.Model.Graph.Operation(inputName[i]).Output(0)] = tensor
+	}
+	fetches := []tf.Output{
+		m.Model.Graph.Operation(outNames).Output(0),
+	}
+	return m.Model.Session.Run(feeds, fetches, targets)
 }
 
-// Gin Web FrameWork
-func GinInit() {
-	r := gin.Default()
-	configJsonPath := flag.String("config_json_path", "", "the config.json path")
-	flag.Parse()
-	config := &utils.LLBConfig{DbName: "postgres_test",
-		DbHost: "localhost", DbUser: "postgres", DbPass: "postgres@"}
-	if len(*configJsonPath) > 0 {
-		configData, err := ioutil.ReadFile(*configJsonPath)
-		check(err)
-		err = json.Unmarshal(configData, &config)
-		check(err)
+func tfModelLoadAndEval() {
+	const MAXLEN int = 20
+	// 将文本转换为id序列，为了实验方便直接使用转换好的ID序列即可
+	inputData := [2][MAXLEN]float32{{1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 208.0, 659.0, 180.0, 408.0, 42.0, 547.0, 829.0, 285.0, 334.0, 42.0, 642.0, 81.0, 800.0}, {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 208.0, 659.0, 180.0, 408.0, 42.0, 547.0, 829.0, 285.0, 334.0, 42.0, 642.0, 81.0, 800.0}}
+	tensor, err := tf.NewTensor(inputData)
+	dropOutTensor, _ := tf.NewTensor(false)
+	if err != nil {
+		fmt.Printf("Error NewTensor: err: %s", err.Error())
+		return
 	}
-	utils.Init()
-	// the jwt middleware
-	authMiddleware := &jwt.GinJWTMiddleware{
-		Realm:      "casem zone",
-		Key:        []byte("this is casemind go api"),
-		Timeout:    time.Hour,
-		MaxRefresh: time.Hour,
-		Authenticator: func(userName string, password string, c *gin.Context) (string, bool) {
-			/**
-			user, err := userModel.SigninEx(userName, password)
-			if err != nil {
-				return "", false
-			}
-			return strconv.Itoa(user.ID), true
-			*/
-			return "", false
-		},
-		Authorizator: func(userId string, c *gin.Context) bool {
-			return true
-		},
-		Unauthorized: func(c *gin.Context, code int, message string) {
-			c.JSON(code, gin.H{
-				"code":    code,
-				"message": message,
-			})
-		},
-		// TokenLookup is a string in the form of "<source>:<name>" that is used
-		// to extract token from the request.
-		// Optional. Default value "header:Authorization".
-		// Possible values:
-		// - "header:<name>"
-		// - "query:<name>"
-		// - "cookie:<name>"
-		TokenLookup: "header:Authorization",
-		// TokenLookup: "query:token",
-		// TokenLookup: "cookie:token",
-
-		// TokenHeadName is a string in the header. Default value is "Bearer"
-		TokenHeadName: "Token",
-
-		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
-		TimeFunc: time.Now,
+	model := Init("/Users/devops/workspace/gitlab/idmg/lieluobo/llblibrary/tfmodel/models/cnnModel", []string{"myTag"})
+	result, err := model.Eval([]*tf.Tensor{tensor, dropOutTensor}, []string{"input_layer_input", "dropout_layer1/keras_learning_phase"}, "output_layer/Sigmoid", nil)
+	if err != nil {
+		fmt.Printf("Error running the session with input, err: %s  ", err.Error())
+		return
 	}
-	r.Use(CORSMiddleware())
-	r.POST("/api/login", authMiddleware.LoginHandler)
-	apiAuth := r.Group("/api/auth")
-	apiAuth.Use(authMiddleware.MiddlewareFunc())
-	apiAuth.GET("/refresh_token", authMiddleware.RefreshHandler)
-
-	// db.Init()
-
-	apiUser := r.Group("/api/user")
-	{
-		apiUser.GET("/index", helloGinHandler)
+	value := result[0].Value().([][]float32)
+	var predict = make([]float32, len(inputData))
+	// 输出结果，interface{}格式
+	fmt.Printf("Result value:%v", value)
+	for i := range inputData {
+		predict[i] = value[i][0]
 	}
-
-	r.LoadHTMLGlob("./public/*.html")
-
-	r.Static("/static", "./casem/case_go/public/static")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		c.HTML(404, "index.html", gin.H{})
-	})
-
-	r.Run(":20989")
+	fmt.Printf("predict value:%v", predict)
 }
 
 func main() {
+	tfModelLoadAndEval()
 }
